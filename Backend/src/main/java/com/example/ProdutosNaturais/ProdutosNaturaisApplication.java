e6f84c1fcc42eaae1c4739d5bdfab53aa753d3df
unknown <joaopetrussousa@gmail.com>
2021-02-10 20:34:34 -0300

    Project created

---
+++ Backend/src/main/java/com/example/ProdutosNaturais/ProdutosNaturaisApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProdutosNaturais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosNaturaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosNaturaisApplication.class, args);
+	}
+
+}
